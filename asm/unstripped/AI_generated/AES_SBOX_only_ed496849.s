d7ff619da9b1e1964411e37009033f1b3933be31
Meishty Pande <meishtypande@gmail.com>
2025-11-07 05:30:55 +0000

    Include SBOX only C program

---
+++ asm/unstripped/AI_generated/AES_SBOX_only_ed496849.s
@@ -0,0 +1,18 @@
+
+Function main @ 0x00400005
+0x00400005:	ldr	r1, [pc, #0x10]
+0x00400007:	movs	r2, #0x63
+0x00400009:	push	{r3, lr}
+0x0040000b:	movs	r0, #1
+0x0040000d:	add	r1, pc
+0x0040000f:	bl	#0x500001
+0x00400013:	movs	r0, #0
+0x00400015:	pop	{r3, pc}
+
+Function sub_400017 @ 0x00400017
+
+Function __printf_chk @ 0x00500001
+0x00500001:	movs	r0, r0
+0x00500003:	movs	r0, r0
+0x00500005:	movs	r0, r0
+0x00500007:	movs	r0, r0
